7b83b95c1818669f82b0a6ddae77d6df34eb1e07
kid84 <84takuya@gmail.com>
2014-03-04 21:37:06 +0900

    d3 sinatra setting

---
+++ Gemfile
@@ -0,0 +1,16 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem "sinatra",require:"sinatra/base"
+gem "sinatra-contrib",require:"sinatra/contrib/all"
+
+gem "haml"
+gem "sass"
+gem "compass"
+gem "coffee-script"
+gem "rdiscount"
+
+group :develoopment do
+	gem "thin"
+end
+
---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'compass'
+
+class App < Sinatra::Base
+	register Sinatra::Reloader
+	
+	# Compassの設定
+	configure do
+    	Compass.configuration do |config|
+      		config.project_path = File.dirname(__FILE__)
+      		config.sass_dir = 'views'
+    	end
+    	set :haml, { format: :html5 }
+    	set :sass, Compass.sass_engine_options
+    	set :scss, Compass.sass_engine_options
+    end
+    
+    get '/' do
+    	haml:'index'
+    end
+    get '/style.css' do
+    	scss:'style'
+    end
+end
