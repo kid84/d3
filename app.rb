fe53a138b0e23c4c3bba712993e9f43878b5def4
kid84 <84takuya@gmail.com>
2014-03-18 21:57:08 +0900

    データベース接続(未完)

--- app.rb
+++ app.rb
@@ -1,9 +1,27 @@
 require 'sinatra'
 require 'compass'
 require 'mongo'
+require 'nokogiri'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+    "adapter"=>"sqlite3",
+    "database"=>"./mmg.db"
+)
+
+class Maildata < ActiveRecord::Base
+end
 
 class App < Sinatra::Base
 	register Sinatra::Reloader
+    # register Sinatra::ActiveRecordExtension
+    # ActiveRecord::Base.establish_connection(
+    # "adapter"=>"sqlite3",
+    # "database"=>"./mmg.db"
+    # )
+
+    # class Maildata < ActiveRecord::Base
+    # end
 	
 	# Compassの設定
 	configure do
@@ -15,9 +33,15 @@ class App < Sinatra::Base
     	set :sass, Compass.sass_engine_options
     	set :scss, Compass.sass_engine_options
     end
-    
+
+    #view部分
     get '/' do
-    	haml:'index'
+        @maildata = Maildata.all
+    	haml:index
+    end
+    post '/new' do
+        # Comment
+        redirect '/'
     end
     get '/style.css' do
     	scss:'style',:style => :expanded,:line_comments => false,:cache=>false
